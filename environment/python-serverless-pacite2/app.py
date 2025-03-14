048adc70344c5beee92e6045f02aa2c6a5ea9521
Arabella Pacite <pacitearabella02@gmail.com>
2025-03-14 10:03:40 +0800

    Working API Call

--- environment/python-serverless-pacite2/app.py
+++ environment/python-serverless-pacite2/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, render_template
+import boto3
+from flask import Flask, render_template, jsonify
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
@@ -7,8 +12,22 @@ def home():
     return render_template('index.html')
 
 @app.route('/products')
-def index():
+def products_page():
     return render_template('products.html')
+
+@app.route('/api/products')
+def get_products():
+    # Configure AWS with environment variables
+    dynamodb = boto3.resource('dynamodb',
+        region_name='us-east-2',
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY_ID'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_ACCESS_KEY')
+    )
+    
+    table = dynamodb.Table('products-pacite2')
+    response = table.scan()
+    
+    return jsonify(response['Items'])
 
 if __name__ == '__main__':
     app.run(debug=True)
